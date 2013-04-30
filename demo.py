5f6c4b31eeea62d4bb165d56081c0ba65b5eb7f1
SEIAROTg <seiarotg@gmail.com>
2013-04-30 16:24:44 +0800

    alpha version

---
+++ demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Sorry, the comments are all in Chinese because this project highly relies on Chinese Language.
+#
+
+import Ironman, wordslist
+
+if __name__ == "__main__":
+	instance = Ironman.Ironman(wordslist.wordslist)
+	print instance.generate()
+	print instance.generate(0x10, 0x10)
